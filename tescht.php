fa2b1e8279111d3fd4f7eedbc0b534f5cc330809
Eddy Wolbert <info@eddy-wolbert.de>
2016-11-02 17:10:47 +0100

    tescht.php um eigene lokale Methode erweitert

--- tescht.php
+++ tescht.php
@@ -6,3 +6,9 @@ public function hallo1()
   $z = $x + $y;
   return $z;
 }
+
+public function lokaleFunktion() 
+{
+	$hallo = "Ich bin eine lokal belegte Variable";
+	return 'Ausgabe ist: ' . $hallo . "!";
+}
